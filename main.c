96df62dae56b3109dc17eaf2f52ebc56bfb6f508
TomzinM <tomzinhom@gmail.com>
2026-03-19 09:34:41 -0300

    Adicionado Turma, Comentario de nome, Print de nome, Pausa de tela, Novo print de nome

--- main.c
+++ main.c
@@ -1,5 +1,34 @@
-#include<stdio.h>
+/*
+Turma: CCPI
 
-int main(){
+Nome: Miguel Marcelo Alves Ramos de Oliveira
+RM: 569467
+
+Nome: Felipe de Oliveira Doern
+RM: 568798
+
+Nome: Eric dos Santos Mendes da Silva
+RM: 569528
+
+Nome: Tom Stringasci Albuquerque Coelho Morais
+RM: 568844
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    printf("Olá, professor!\n");
+
+    printf("Miguel Marcelo Alves Ramos de Oliveira\n");
+
+    printf("Felipe de Oliveira Doern\n"); 
+
+    printf("Eric dos Santos Mendes da Silva\n");
+
+    printf("Tom Stringasci Albuquerque Coelho Morais\n");
+
+    system("pause");
     return 0;
 }
